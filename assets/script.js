a018a6061fcc77fa06577cd2e3746604459cd8ac
Neoalina <neo-alina@yandex.ru>
2022-01-22 13:56:11 +0300

    Created registration form

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+
+function clickMe() {
+    let lastName = document.getElementById('floatingLastName').value;
+    let email = document.getElementById('floatingInput').value;
+    let password = document.getElementById('floatingPassword').value;
+    let firstName = document.getElementById('floatingFirstName').value;
+    
+    document.getElementById('errormessage').innerHTML= '';
+    if (email=" 1 ") {
+        document.getElementById('errormessage').innerHTML+="Это адрес <br>";
+    } else {
+        document.getElementById('errormessage').innerHTML+="Ведите правильный адрес <br>";
+    }
+    
+    
+    if (password.length<=5 && password.length>0) {
+        document.getElementById('errormessage').innerHTML+="Слишком короткий пароль <br>";
+    }
+    
+    if (firstName && lastName && email && password) {
+        document.getElementById('errormessage').innerHTML+=(`Добро пожаловать, ${firstName}! <br>`);
+    } else {
+        document.getElementById('errormessage').innerHTML+=('Заполните все поля, пожалуйста.<br>');
+    }
+
+    
+}
